9ccea476b1babb7000b97b927f0502e46f857896
Endy Mitsuya Takano <endytkndev@gmail.com>
2024-09-12 23:04:41 -0400

    feat: changing heigh text position and renaming title manager class pacakge

--- src/main/kotlin/endytkn/randomEvents/titleManager/TitleManager.kt
+++ src/main/kotlin/endytkn/randomEvents/titleManager/TitleManager.kt
@@ -1,16 +1,13 @@
-package example.examplemod.event
+package endytkn.randomEvents.titleManager
 
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Font
-import net.minecraftforge.api.distmarker.Dist
-import net.minecraftforge.client.event.ClientChatEvent
 import net.minecraftforge.client.event.RenderGuiOverlayEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 
-@Mod.EventBusSubscriber(modid = "examplemod", bus = Mod.EventBusSubscriber.Bus.FORGE, value = [Dist.CLIENT])
-object ModEvents {
-
+@Mod.EventBusSubscriber
+object TitleManager {
     private var displayMessage: String? = null
     private var initialDisplayTime: Int = 3000 // 15 seconds (20 ticks = 1 second)
     private var displayTime: Int = 0
@@ -45,14 +42,6 @@ object ModEvents {
         return (alphaValue shl 24) or (red shl 16) or (green shl 8) or blue
     }
 
-    // Capture the chat message and initialize the display time and opacity
-    @SubscribeEvent
-    fun onChatMessage(event: ClientChatEvent) {
-        displayMessage = "Overworld" // Example text
-        displayTime = initialDisplayTime // Reset the display time
-        alpha = 1.0f // Reset opacity to full visibility
-    }
-
     // Render the message on the screen with the fade-out and color transition effect
     @SubscribeEvent
     fun onRenderOverlay(event: RenderGuiOverlayEvent.Post) {
@@ -71,11 +60,11 @@ object ModEvents {
 
             // Draw the string on the screen
             event.guiGraphics.pose().pushPose() // Save the current transformation state
-            event.guiGraphics.pose().scale(2.0f, 2.0f, 2.0f) // Scale the font by 2x
+            event.guiGraphics.pose().scale(1.2f, 1.2f, 1.2f) // Scale the font by 2x
 
             // Calculate the X and Y positions without scaling, then scale
             val unscaledX = (screenWidth / 2 - font.width(displayMessage!!) ) // Unscaled X position
-            val unscaledY = (screenHeight / 2) // Unscaled Y position
+            val unscaledY = (screenHeight*0.2) // Unscaled Y position
 
             event.guiGraphics.drawString(
                 font, 
